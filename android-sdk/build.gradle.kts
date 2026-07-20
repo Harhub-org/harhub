93f2f8efdbecd17b2317da15b94e9a95010827cb
hastagaming <rip.hastagaming@gmail.com>
2026-07-20 19:53:30 +0700

    Fix

--- android-sdk/build.gradle.kts
+++ android-sdk/build.gradle.kts
@@ -1,15 +1,5 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0")
-    }
-}
-
 plugins {
-    id("com.android.library") version "8.6.0" apply true
+    id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
 }
